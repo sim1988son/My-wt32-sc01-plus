bd9bc14e2277575105836387c47911a99b0ba6b0
SimpSon <sim1988son@hotmail.com>
2022-11-23 17:33:12 +0100

    Add files via upload

---
+++ src/hadware/json_psram_allocator.h
@@ -0,0 +1,31 @@
+#ifndef SPI_RAM_JSON_DOC_H_
+#define SPI_RAM_JSON_DOC_H_
+
+//#include "config.h"
+#include "ArduinoJson.h"
+#include "alloc.h"
+
+// arduinoJson allocator for external PSRAM
+// see: https://arduinojson.org/v6/how-to/use-external-ram-on-esp32/
+struct SpiRamAllocator {
+    void* allocate( size_t size ) {
+        if ( size == 0 ) {
+            log_e("allocate zero bytes? really? abort");
+            return( NULL );
+        }
+        void *ram = MALLOC( size );
+        if ( ram ) {
+            return( ram );
+        }
+        else {
+            log_e("allocate %d bytes (%p) json psram failed", size, ram );
+            while(1);
+        }
+    }
+    void deallocate( void* pointer ) {
+        free( pointer );
+    }
+};
+using SpiRamJsonDocument = BasicJsonDocument<SpiRamAllocator>;
+
+#endif
